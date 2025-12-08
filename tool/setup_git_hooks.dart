7a609934bbce3e0bb5a79e64f39c8077e3697b73
Cedric Ziel <cedric@cedric-ziel.com>
2025-12-08 14:00:09 +0100

    fix: use flutter pub run for pre-commit hook
    Switch from dart run to flutter pub run for dart_pre_commit
    to properly resolve dependencies in the Flutter project context.

--- tool/setup_git_hooks.dart
+++ tool/setup_git_hooks.dart
@@ -5,8 +5,8 @@ Future<void> main() async {
   await preCommitHook.parent.create();
   await preCommitHook.writeAsString('''
 #!/bin/sh
-exec dart run dart_pre_commit # specify custom options here
-# exec flutter pub run dart_pre_commit # Use this instead when working on a flutter project
+# exec dart run dart_pre_commit # specify custom options here
+exec flutter pub run dart_pre_commit # Use this instead when working on a flutter project
 ''');
 
   if (!Platform.isWindows) {
